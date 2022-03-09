9ad08d5240a604aa074bb5f7759b4cac7fdd2bc3
Vu Pham <vupham25296@gmail.com>
2022-03-09 20:42:14 +0700

    update

---
+++ src/main/java/com/example/swp490_g25_sse/dto/AccountInfoDto.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.swp490_g25_sse.dto;
+
+/**
+ *
+ * @author vupham
+ */
+public class AccountInfoDto {
+    private String currentPassword;
+    private String newPassword;
+    private String confirmNewPassword;
+    
+    public AccountInfoDto() {
+       
+    }
+    public AccountInfoDto(String currentPassword, String newPassword, String confirmNewPassword) {
+        this.currentPassword = currentPassword;
+        this.newPassword = newPassword;
+        this.confirmNewPassword = confirmNewPassword;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmNewPassword() {
+        return confirmNewPassword;
+    }
+
+    public void setConfirmNewPassword(String confirmNewPassword) {
+        this.confirmNewPassword = confirmNewPassword;
+    }
+    
+ 
+}
