8d4728ded495bb3f26a97ef036ad4bf1a33c2ed0
bettafish15 <bettafish6@gmail.com>
2022-03-16 22:06:17 +0700

    finally

---
+++ src/main/resources/db/migration/V1_0_4__retype_column.sql
@@ -0,0 +1,8 @@
+alter table courses
+       alter column content varchar(max);
+
+alter table lectures 
+       alter column content varchar(max);
+
+alter table tests 
+       alter column content varchar(max);
